d3a060bf3b1234cfd5ecb9e3ba742fc067c5b36e
Rupesh-roopz <rupesh.esambadi@aspiresys.com>
2022-01-21 11:05:24 +0530

    initial commit with added columns

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TableDataGrid from './components/table';
+
+function App() {
+  return (
+    <div className="App">
+      <TableDataGrid />
+    </div>
+  );
+}
+
+export default App;
